ff3d5ce2d3d510f57655e7a98a697b33ace50ca9
unknown <APL93194@APL93194-PC.apollopharmacy.org>
2025-10-07 17:39:22 +0530

    Updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-const Card = ({title})=>{
+const App = () => {
   return (
-    <div className='card' style={{border:"1px solid #4b5362",
-      padding:"20px",margin:"10px",backgroundColor:"#31363f",
-      borderRadius:"10px",minHeight:'100px'
-    }}>
-      <h2>{title}</h2>
+    <div>
+      <h1 class="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
     </div>
   )
-}
-
-const App = ()=>{
-  const [] = useState()
-    return ( 
-      <div className='card-container'>
-        <h2> functional arrow component</h2>
-        <Card title="star wars" />
-        <Card title="lion king"/>
-        <Card title="cars"/>
-      </div>
-    )
 }
 
 export default App
